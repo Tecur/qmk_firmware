764610c5d2a841c8d9c9aaece3a2bc92b3f42b79
Tecur <joshua.stoll@web.de>
2020-09-22 01:26:19 +0200

    Update keymap.c

--- keyboards/ai03/polaris/keymaps/tecur/keymap.c
+++ keyboards/ai03/polaris/keymaps/tecur/keymap.c
@@ -17,14 +17,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all( /* Base */
     KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_BSPC, _______,
     KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE,   DE_PLUS,          DE_HASH,
-    KC_CAPS, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE,                     _______,
+    KC_CAPS, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE,                     KC_ENT,
     KC_LSFT, DE_LESS, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT,          MO(_FN),
     KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           KC_SPC,           KC_SPC,                    KC_RALT, KC_RGUI, KC_APP,  KC_RCTL
   ),
   [_FN] = LAYOUT_all( /* FN */
     RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
-    KC_WHOM, KC_MYCM, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT,          _______,
-    _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______,                   _______,
+    DE_CIRC, KC_MYCM, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT,          _______,
+    KC_WHOM, KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______,                   _______,
     KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PSCR, _______,
     KC_PGDN, TS_MUTE, TS_DEAF,          _______,          _______,          _______,                   _______, _______, _______, _______
   )
