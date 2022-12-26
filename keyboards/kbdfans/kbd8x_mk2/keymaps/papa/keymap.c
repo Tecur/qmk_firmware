9ef6c9c4eab97c6867c35a9ea114f4e54159a04f
Joshua Stoll <joshua.stoll@web.de>
2022-12-26 01:04:25 +0100

    Changed papa Keymap and config

--- keyboards/kbdfans/kbd8x_mk2/keymaps/papa/keymap.c
+++ keyboards/kbdfans/kbd8x_mk2/keymaps/papa/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "keymap_german.h"
+#include "keymap_swedish.h"
 
 //Layer names
 enum layer_names {
@@ -7,33 +8,40 @@ enum layer_names {
     FN
 };
 
-enum unicode_names {
-    FRAC12,
-    FRAC14,
-    FRAC34
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [FRAC12]  = 0x00BD,  // ½
-    [FRAC14] = 0x00BC,  // ¼
-    [FRAC34]  = 0x00BE, // ¾
+enum custom_keycodes {
+  CTRL_M = SAFE_RANGE,
+  //add more here...
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE] = LAYOUT_tkl_iso(
+    [BASE] = LAYOUT_tkl_iso_split(
         KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS, \
         DE_CIRC, DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_BSPC,          KC_INS,  KC_HOME, KC_PGUP, \
         KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE,   DE_PLUS,                   KC_DEL,  KC_END,  KC_PGDN, \
         KC_CAPS, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE,   DE_HASH, KC_ENT,                                      \
-        KC_LSFT, DE_LESS, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS,          KC_RSFT,                   KC_UP,            \
-        KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                              KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LSFT, DE_LESS, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT, CTRL_M,                    KC_UP,            \
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(FN),  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
-    [FN] = LAYOUT_tkl_iso(
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS, \
-        DE_CIRC, DE_1,    X(FRAC12),    DE_3,    XP(FRAC14,FRAC34),    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_BSPC,          KC_INS,  KC_HOME, KC_PGUP, \
-        KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE,   DE_PLUS,                   KC_DEL,  KC_END,  KC_PGDN, \
-        KC_CAPS, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE,   DE_HASH, KC_ENT,                                      \
-        KC_LSFT, DE_LESS, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS,          KC_RSFT,                   KC_UP,            \
-        KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                              KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
+    [FN] = LAYOUT_tkl_iso_split(
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, \
+        _______, _______, SE_HALF, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                     \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,          \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______,          _______, _______, _______  \
     )
+};
+
+//Macro events
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case CTRL_M:                                   // CTRL + M
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL("M"));
+      } else {
+      }
+      break;
+      //add more here...
+  }
+  return true;  //for use later
 };
